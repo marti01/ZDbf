6a68a25e44324e0f68373ee85293c9883e852616
Marti PIntó <m.pinto@gmail.com>
2021-03-08 12:18:39 +0100

    v0.1 - Funciona

---
+++ app.js
@@ -0,0 +1,19 @@
+//import {DBFFile} from 'dbffile';
+const {DBFFile} = require('dbffile');
+ 
+async function testRead() {
+    try {
+        let dbf = await DBFFile.open('Transcat_carretera_perill_trams.dbf');
+        console.log(`DBF file contains ${dbf.recordCount} records.`);
+        console.log(`Field names: ${dbf.fields.map(f => f.name).join(', ')}`);
+        let records = await dbf.readRecords(100)
+        //for (let record of records) console.log(records);
+    } catch(e) {
+        console.log("Error: "+e);
+    }
+}
+
+   testRead();
+
+
+ 
